9b5302f43398ec317beb27d66c9e2d9357f79960
andberger <and.bergsson@gmail.com>
2019-03-04 22:04:51 +0000

    Replace missing numerical values with mean

--- work/script.py
+++ work/script.py
@@ -6,30 +6,34 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('dataCase1.csv')
 
 # data preparation
-
-# Numerical nan's: replace missing values with the mean.
-
-# Categorical nan's: replace the missing entries with the most frequent one.
 def get_most_common_value(column):
     return df[column].value_counts().head(1).index.values[0]
 
-def replace_categorical_nan(x, most_common_value):
+def replace_nan(x, replace_value):
     if pd.isnull(x):
-        return most_common_value
+        return replace_value
     else:
         return x
 
+# Numerical nan's: replace missing values with the mean.
+columns = df.columns[1:]
+for i in range(len(columns) - 5):
+    mean = df[columns[i]].mean()
+    df[columns[i]] = df[columns[i]].map(lambda x: replace_nan(x, mean))
+
+# Categorical nan's: replace the missing entries with the most frequent one.
+#TODO: put in a loop
 x96_mcv = get_most_common_value('X96') #B
 x97_mcv = get_most_common_value('X97') #B
 x98_mcv = get_most_common_value('X98') #D
 x99_mcv = get_most_common_value('X99') #D
 x100_mcv = get_most_common_value('X100') #D
 
-df['X96'] = df['X96'].map(lambda x: replace_categorical_nan(x, x96_mcv))
-df['X97'] = df['X97'].map(lambda x: replace_categorical_nan(x, x97_mcv))
-df['X98'] = df['X98'].map(lambda x: replace_categorical_nan(x, x98_mcv))
-df['X99'] = df['X99'].map(lambda x: replace_categorical_nan(x, x98_mcv))
-df['X100'] = df['X100'].map(lambda x: replace_categorical_nan(x, x100_mcv))
+df['X96'] = df['X96'].map(lambda x: replace_nan(x, x96_mcv))
+df['X97'] = df['X97'].map(lambda x: replace_nan(x, x97_mcv))
+df['X98'] = df['X98'].map(lambda x: replace_nan(x, x98_mcv))
+df['X99'] = df['X99'].map(lambda x: replace_nan(x, x98_mcv))
+df['X100'] = df['X100'].map(lambda x: replace_nan(x, x100_mcv))
 
 
 
